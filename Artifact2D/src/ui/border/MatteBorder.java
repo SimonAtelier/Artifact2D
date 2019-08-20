1b7a76a39b7d5d9a5f688e7d4888ef1453d237da
Simon <Simon@LAPTOP-NAT2JJES.speedport.ip>
2019-08-21 00:29:59 +0200

    refactoring extracted methods and members

--- Artifact2D/src/ui/border/MatteBorder.java
+++ Artifact2D/src/ui/border/MatteBorder.java
@@ -5,37 +5,63 @@ import java.awt.Graphics2D;
 
 public class MatteBorder extends AbstractBorder {
 
+	private int x;
+	private int y;
+	private int width;
+	private int height;
 	private Insets insets;
 	private Color color;
+	private Graphics2D g2d;
 
 	public MatteBorder(int top, int left, int bottom, int right, Color color) {
 		insets = new Insets(top, left, bottom, right);
 		this.color = color;
 	}
 
-	private void renderTopBorder(Graphics2D g2d, int x, int y, int width, int height) {
+	private void renderTopBorder() {
 		g2d.fillRect(x + insets.left, y, width - insets.left - insets.right, insets.top);
 	}
 
-	private void renderBottomBorder(Graphics2D g2d, int x, int y, int width, int height) {
+	private void renderBottomBorder() {
 		g2d.fillRect(x + insets.left, y + height - insets.bottom, width - insets.left - insets.right, insets.bottom);
 	}
 
-	private void renderLeftBorder(Graphics2D g2d, int x, int y, int width, int height) {
+	private void renderLeftBorder() {
 		g2d.fillRect(x, y, insets.left, height);
 	}
 
-	private void renderRightBorder(Graphics2D g2d, int x, int y, int width, int height) {
+	private void renderRightBorder() {
 		g2d.fillRect(x + width - insets.right, y, insets.right, height);
+	}
+	
+	private void setBorderFrame(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	private void setGraphicsContext(Graphics2D g2d) {
+		this.g2d = g2d;
+	}
+	
+	private void setColor() {
+		g2d.setColor(color);
+	}
+	
+	private void renderBorderParts() {
+		renderTopBorder();
+		renderBottomBorder();
+		renderLeftBorder();
+		renderRightBorder();
 	}
 
 	@Override
 	public void renderBorder(Graphics2D g2d, int x, int y, int width, int height) {
-		g2d.setColor(color);
-		renderTopBorder(g2d, x, y, width, height);
-		renderBottomBorder(g2d, x, y, width, height);
-		renderLeftBorder(g2d, x, y, width, height);
-		renderRightBorder(g2d, x, y, width, height);
+		setBorderFrame(x, y, width, height);
+		setGraphicsContext(g2d);
+		setColor();
+		renderBorderParts();
 	}
 
 	@Override
