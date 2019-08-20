aca3c7afe1ccce1d4fa3cea7c22e5743697b6bd5
Simon <Simon@LAPTOP-NAT2JJES.speedport.ip>
2019-08-21 00:14:58 +0200

    refactoring

--- Artifact2D/src/ui/examples/BorderExample.java
+++ Artifact2D/src/ui/examples/BorderExample.java
@@ -4,8 +4,9 @@ import java.awt.Color;
 
 import game.Game;
 import ui.Panel;
-import ui.UiElement;
+import ui.border.Border;
 import ui.border.BorderFactory;
+import ui.border.CornerGapBorder;
 import ui.layout.Anchor;
 import ui.layout.GridLayout;
 
@@ -17,56 +18,50 @@ public class BorderExample extends Game {
 		game.start();
 	}
 	
+	private Panel mainPanel;
+	
 	@Override
 	public void onInitialize() {
+		initializeMainPanel();
+		addBorderPanels();
+		getUiRoot().add(mainPanel);
+	}
+	
+	private void initializeMainPanel() {
+		mainPanel = createMainPanel();
+	}
+	
+	private void addBorderPanels() {
+		addBorderPanel(BorderFactory.createBevelBorder(Color.LIGHT_GRAY, Color.DARK_GRAY));
+		addBorderPanel(BorderFactory.createCornerSquaresBorder(5, Color.WHITE));
+		addBorderPanel(BorderFactory.createCornerSquaresInlinedBorder(5, Color.WHITE, Color.GRAY));
+		addBorderPanel(BorderFactory.createKeyLabelBorder(Color.DARK_GRAY, Color.LIGHT_GRAY));
+		addBorderPanel(BorderFactory.createLineBorder(Color.WHITE));
+		addBorderPanel(BorderFactory.createLoweredEtchedBorder(Color.LIGHT_GRAY, Color.DARK_GRAY));
+		addBorderPanel(BorderFactory.createMatteBorder(4, 2, 1, 10, Color.WHITE));
+		addBorderPanel(BorderFactory.createMessageBoxBorder(Color.WHITE, Color.BLACK));
+		addBorderPanel(BorderFactory.createRaisedEtchedBorder(Color.LIGHT_GRAY, Color.DARK_GRAY));
+		addBorderPanel(BorderFactory.createTitledBorder("Untitled", 2, Color.GRAY, Color.WHITE));
+		addBorderPanel(new CornerGapBorder(4, Color.LIGHT_GRAY));
+	}
+	
+	private Panel createMainPanel() {
 		Panel mainPanel = new Panel();
 		mainPanel.setBackground(new Color(20, 20, 20));
 		mainPanel.setLayoutAnchor(Anchor.FULL_RECTANGLE);
 		mainPanel.setBorder(BorderFactory.createEmptyBorder(10));
-		mainPanel.setLayout(new GridLayout(5, 2, 5, 5));
-		
-		Panel panel0 = new Panel();
-		panel0.setBorder(BorderFactory.createBevelBorder(Color.LIGHT_GRAY, Color.DARK_GRAY));	
-		mainPanel.add(panel0);
-		
-		Panel panel1 = new Panel();
-		panel1.setBorder(BorderFactory.createCornerSquaresBorder(5, Color.WHITE));	
-		mainPanel.add(panel1);
-		
-		Panel panel2 = new Panel();
-		panel2.setBorder(BorderFactory.createCornerSquaresInlinedBorder(5, Color.WHITE, Color.GRAY));	
-		mainPanel.add(panel2);
-		
-		Panel panel3 = new Panel();
-		panel3.setBorder(BorderFactory.createKeyLabelBorder(Color.DARK_GRAY, Color.LIGHT_GRAY));
-		mainPanel.add(panel3);
-		
-		Panel panel4 = new Panel();
-		panel4.setBorder(BorderFactory.createLineBorder(Color.WHITE));
-		mainPanel.add(panel4);
-		
-		Panel panel5 = new Panel();
-		panel5.setBorder(BorderFactory.createLoweredEtchedBorder(Color.LIGHT_GRAY, Color.DARK_GRAY));
-		mainPanel.add(panel5);
-		
-		Panel panel6 = new Panel();
-		panel6.setBorder(BorderFactory.createMatteBorder(4, 2, 1, 10, Color.WHITE));
-		mainPanel.add(panel6);
-		
-		Panel panel7 = new Panel();
-		panel7.setBorder(BorderFactory.createMessageBoxBorder(Color.WHITE, Color.BLACK));
-		mainPanel.add(panel7);
-		
-		Panel panel8 = new Panel();
-		panel8.setBorder(BorderFactory.createRaisedEtchedBorder(Color.LIGHT_GRAY, Color.DARK_GRAY));
-		mainPanel.add(panel8);
-		
-		Panel panel9 = new Panel();
-		panel9.setBorder(BorderFactory.createTitledBorder("Untitled", 2, Color.GRAY, Color.WHITE));
-		mainPanel.add(panel9);
-
-		UiElement uiRoot = getUiRoot();
-		uiRoot.add(mainPanel);
+		mainPanel.setLayout(new GridLayout(6, 2, 5, 5));
+		return mainPanel;
+	}
+	
+	private void addBorderPanel(Border border) {
+		mainPanel.add(createBorderPanel(border));
+	}
+	
+	private Panel createBorderPanel(Border border) {
+		Panel panel = new Panel();
+		panel.setBorder(border);
+		return panel;
 	}
 
 }
