19ad17903b38ad69d5c86937ce706c06307a2be3
Simon <Simon@LAPTOP-NAT2JJES.speedport.ip>
2019-08-26 14:42:12 +0200

    refactoring extracted members

--- Artifact2D/src/ui/layout/GridLayout.java
+++ Artifact2D/src/ui/layout/GridLayout.java
@@ -6,20 +6,25 @@ public class GridLayout implements Layout {
 
 	private int rows;
 	private int cols;
+	private int vGap;
+	private int hGap;
+	private int maxRowWidth;
+	private int maxRowHeight;
 
 	public GridLayout(int rows, int cols) {
 		this.rows = rows;
 		this.cols = cols;
 	}
 
+	private void reset() {
+		maxRowWidth = maxRowHeight = vGap = hGap = 0;
+	}
+
 	@Override
 	public void layout(UiElement uiElement) {
+		reset();
+		
 		int childIndex = 0;
-		int vGap = 0;
-		int hGap = 0;
-		int maxRowWidth = 0;
-		int maxRowHeight = 0;
-
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
 				if (childIndex >= uiElement.getChildCount())
