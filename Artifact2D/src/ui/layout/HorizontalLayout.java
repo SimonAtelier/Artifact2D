03f149d1451990841538b549726a6969068f9d0b
Simon <Simon@LAPTOP-NAT2JJES.speedport.ip>
2019-08-26 13:50:22 +0200

    refactoring extracted methods

--- Artifact2D/src/ui/layout/HorizontalLayout.java
+++ Artifact2D/src/ui/layout/HorizontalLayout.java
@@ -4,35 +4,115 @@ import ui.UiElement;
 
 public class HorizontalLayout implements Layout {
 
+	private int layoutX;
+	private int minimumHeight;
+	private int minimumWidth;
+	private UiElement parent;
+	private UiElement child;
+
 	@Override
 	public void layout(UiElement uiElement) {
-		int offsetX = 0;
-		int width = 0;
-		int maxHeight = 0;
-		
-		for (int i = 0; i < uiElement.getChildCount(); i++) {
-			UiElement child = uiElement.getChildAt(i);
-			child.setLayoutX(offsetX);
-			
-			int childWidth = child.getBorderBoxWidth() + child.getMargin().getHorizontalInsets();
-			int childHeight = child.getBorderBoxHeight() + child.getMargin().getVerticalInsets();
-			
-			offsetX += childWidth;
-			width +=  childWidth;
-			maxHeight = childHeight > maxHeight ? childHeight : maxHeight;
-		}
-		
-		if (width > uiElement.getWidth()) {
-			uiElement.setLayoutWidth(width - uiElement.getWidth());
-		} else {
-			uiElement.setLayoutWidth(0);
-		}
-		
-		if (maxHeight > uiElement.getHeight()) {
-			uiElement.setLayoutHeight(maxHeight - uiElement.getHeight());
-		} else {
-			uiElement.setLayoutHeight(0);
+		reset();
+		setParent(uiElement);
+		layoutChildren();
+		applyLayoutDimensionToParent();
+	}
+
+	private void layoutChildren() {
+		for (int i = 0; i < parent.getChildCount(); i++) {
+			setChild(parent.getChildAt(i));
+			applyLayoutXToChild();
+			updateLayoutX();
+			updateMinimumDimension();
 		}
 	}
 	
+	private void updateMinimumDimension() {
+		updateMinimumWidth();
+		updateMinimumHeight();
+	}
+	
+	private void reset() {
+		layoutX = minimumHeight = minimumWidth = 0;
+	}
+	
+	private void updateLayoutX() {
+		layoutX += getChildWidth();
+	}
+	
+	private void updateMinimumWidth() {
+		int childWidth = getChildWidth();
+		minimumWidth += childWidth;
+	}
+	
+	private void updateMinimumHeight() {
+		int childHeight = getChildHeight();
+		minimumHeight = childHeight > minimumHeight ? childHeight : minimumHeight;
+	}
+	
+	private void applyLayoutDimensionToParent() {
+		setParentLayoutWidth(calculateLayoutWidth());
+		setParentLayoutHeight(calculateLayoutHeight());
+	}
+	
+	private int calculateLayoutWidth() {
+		return needToApplyLayoutWidth() ? getMinimumWidth() - getParentWidth() : 0;
+	}
+	
+	private int calculateLayoutHeight() {
+		return needToApplyLayoutHeight() ? getMinimumHeight() - getParentHeight() : 0;
+	}
+	
+	private boolean needToApplyLayoutWidth() {
+		return getMinimumWidth() > getParentWidth();
+	}
+	
+	private boolean needToApplyLayoutHeight() {
+		return getMinimumHeight() > getParentHeight();
+	}
+	
+	private void applyLayoutXToChild() {
+		child.setLayoutX(layoutX);
+	}
+	
+	private void setParentLayoutWidth(int layoutWidth) {
+		parent.setLayoutWidth(layoutWidth);
+	}
+	
+	private void setParentLayoutHeight(int layoutHeight) {
+		parent.setLayoutHeight(layoutHeight);
+	}
+	
+	private int getParentWidth() {
+		return parent.getWidth();
+	}
+	
+	private int getParentHeight() {
+		return parent.getHeight();
+	}
+	
+	private int getChildWidth() {
+		return child.getBorderBoxWidth() + child.getMargin().getHorizontalInsets();
+	}
+	
+	private int getChildHeight() {
+		return child.getBorderBoxHeight() + child.getMargin().getVerticalInsets();
+	}
+	
+	private void setParent(UiElement uiElement) {
+		this.parent = uiElement;
+	}
+	
+	private void setChild(UiElement child) {
+		this.child = child;
+	}
+	
+	private int getMinimumWidth() {
+		return minimumWidth;
+	}
+	
+	private int getMinimumHeight() {
+		return minimumHeight;
+	}
+
 }
